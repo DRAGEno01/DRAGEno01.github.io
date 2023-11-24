ce2fb20da944be9635d5ddac2cf8c00534e40620
DRAGEno01 <73526439+DRAGEno01@users.noreply.github.com>
2023-11-24 22:39:38 +1100

    Update Version.js

--- Version.js
+++ Version.js
@@ -2,3 +2,9 @@ export let drageGitHubVersion = "Alpha 0.02";
 export let versionData = "0.1";
 export let mkkv = "0.01";
 export let mkk = true;
+
+let mkklet = true;
+let mkkvlet = "0.01";
+
+export default mkklet;
+export default mkkvlet;
