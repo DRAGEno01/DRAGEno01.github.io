e9d27e450ea51fa82165a2c593e87f6c2a25efbc
DRAGEno01 <73526439+DRAGEno01@users.noreply.github.com>
2024-10-09 09:25:15 +1100

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-export var dragechatServerVersion = 0.00005;
+export var dragechatServerVersion = 0.00006;
 export var dragechatAllowUsage = true;
 
 // export var drageGitHubVersion = "Alpha 0.02";
